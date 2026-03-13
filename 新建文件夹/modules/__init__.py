6d930635df5802aa3a21318379b6263cbb7d1675
殉道者 <74334114+mtwlg@users.noreply.github.com>
2026-03-13 17:13:07 +0800

    Delete 新建文件夹 directory

--- 新建文件夹/modules/__init__.py
+++
@@ -1,7 +0,0 @@
-# modules/__init__.py
-"""功能模块包"""
-from .timeshare import TimeShareModule
-from .capital import CapitalModule
-from .details import DetailsModule
-from .announcements import AnnouncementsModule
-from .research import ResearchModule
--- 新建文件夹/widgets/__init__.py
+++
@@ -1,4 +0,0 @@
-# widgets/__init__.py
-"""自定义组件包"""
-from .chart_widget import TimeShareChart
-from .info_display import InfoDisplay, TabView
